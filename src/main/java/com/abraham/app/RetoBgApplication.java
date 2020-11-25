560dfa18d1dadbbec405b93d9a2519472fe20e19
Abraham <abrahamvega987@gmail.com>
2020-11-25 01:41:12 -0500

    Initial commit

---
+++ src/main/java/com/abraham/app/RetoBgApplication.java
@@ -0,0 +1,13 @@
+package com.abraham.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoBgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoBgApplication.class, args);
+	}
+
+}

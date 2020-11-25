f940836ae65485e0337bb19950fce45f2579c3f2
Abraham <abrahamvega987@gmail.com>
2020-11-25 03:18:22 -0500

    Rest Api working

---
+++ src/main/java/com/abraham/app/entity/User.java
@@ -0,0 +1,112 @@
+package com.abraham.app.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3631263454440344353L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 50,nullable=false)
+	private String name;
+	private String username;
+	
+	@Column(nullable=false,length=50,unique=true)
+	private String email;
+	
+	@OneToOne(cascade= {CascadeType.ALL})
+    @JoinColumn(name = "address_id")
+	private Address address;
+	
+	@Column(nullable=false,length=50,unique=true)
+	private String phone;
+	private String website;
+	
+	@OneToOne(cascade= {CascadeType.ALL})
+    @JoinColumn(name = "company_id")
+	private Company company;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+	
+}
